62bd60e19c77c4aa07f56358fd993a29f2c9ecf9
Ahmed Bayomy <ahmedhelal20040407@gmail.com>
2026-09-25 15:58:59 +0300

    news screen

--- lib/view/widgets/news_item.dart
+++ lib/view/widgets/news_item.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class NewsItem extends StatelessWidget {
-  const new({super.key});
+  const new({
+    super.key,
+    required this.continent,
+    required this.image,
+    required this.title,
+  });
+  final String continent;
+  final String image;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 32),
+      child: Column(
+        spacing: 8,
+        crossAxisAlignment: .start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(8),
+            child: Image.network(image),
+          ),
+          Text(continent, style: Theme.of(context).textTheme.bodySmall),
+          Text(title, style: Theme.of(context).textTheme.bodyMedium),
+        ],
+      ),
+    );
   }
 }
